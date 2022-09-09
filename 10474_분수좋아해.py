eead9bedec67ee57106665b3417ff71b31a39088
delena0702 <57484961+delena0702@users.noreply.github.com>
2022-09-09 21:43:23 +0900

    22.09.09 수정

--- 10474_분수좋아해.py
+++
@@ -1,8 +0,0 @@
-import sys
-def input(): return sys.stdin.readline().strip()
-
-while True:
-    a, b = map(int, input().split())
-    if a == b == 0:
-        break
-    print(f"{a // b} {a - (a // b) * b} / {b}")
