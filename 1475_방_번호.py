a5525c821314b6daa625201e944806179a2594d7
delena0702 <delena0702@gmail.com>
2022-09-20 22:50:05 +0900

    22.09.20 (Edit)

--- 1475_방_번호.py
+++
@@ -1,7 +0,0 @@
-from collections import defaultdict
-
-N, cnt = input(), defaultdict(int)
-for ch in N:
-    cnt[int(ch)] += 1
-cnt[6] = cnt[9] = (cnt[6] + cnt[9] + 1) // 2
-print(max(cnt.values()))
